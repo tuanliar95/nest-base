1d40dbd1867f8075e9d6624dea74564f85f4b82c
anhvt <tuanliar.95@gmail.com>
2025-03-11 22:47:56 +0700

    them firebase

---
+++ firebase.ts
@@ -0,0 +1,8 @@
+export const CONFIG = {
+    apiKey: 'AIzaSyDvt5D2QN7uqdhtp4ZL9VR8y5wRe228o20',
+    authDomain: 'my-store-5fa75.firebaseapp.com',
+    projectId: 'my-store-5fa75',
+    storageBucket: 'my-store-5fa75.firebasestorage.app',
+    messagingSenderId: '331764581660',
+    appId: '1:331764581660:web:d84e59d3b6fd4607f466d9',
+}
