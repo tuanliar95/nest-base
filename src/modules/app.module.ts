3cc6ebf7ffa10ce844845867e768fc36adfcfe2e
anhvt45 <anhvt45@email.com>
2025-03-17 11:32:31 +0700

    fix JwtModule

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -4,13 +4,18 @@ import { JwtStrategy } from 'src/auth/jwt.strategy';
 import * as controllers from 'src/controllers';
 import * as services from 'src/services';
 import { FirebaseModule } from './firebase.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     FirebaseModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1h' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [...Object.values(controllers)],
